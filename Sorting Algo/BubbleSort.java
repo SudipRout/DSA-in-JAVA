6bf05237546a86677b43487dfe3042ff59436285
SudipRout <sudip.rout007@gmail.com>
2023-07-19 12:36:39 +0530

    Sort changes

---
+++ Sorting Algo/BubbleSort.java
@@ -0,0 +1,36 @@
+public class BubbleSort {
+
+	public static void main(String[] args) {
+	    int nums[] = {7, 1, 5, 3, 6, 2, 4}; //O(n)
+        System.out.println("After sorting the array elements are: ");
+	    bubbleSort(nums);
+	    printArr(nums);
+	}
+	
+	public static void bubbleSort(int nums[]) {
+
+        for (int turn = 0; turn < nums.length-1; turn++) {
+            boolean swapped = false;
+            for (int j = 0; j < nums.length-1-turn; j++) {
+                
+                if (nums[j] > nums[j+1]) {
+                    //swap
+                    int temp = nums[j];
+                    nums[j] = nums[j+1];
+                    nums[j+1] = temp;
+                    swapped = true;
+                }
+            }
+            if(swapped == false)
+            //already sorted
+            break;
+        }
+	}
+	
+	public static void printArr(int nums[]) {
+	    for (int i=0; i<nums.length; i++) {
+	        System.out.print(nums[i]+" ");
+	    }
+	    System.out.println();
+    }
+}
